476176106cc75ab6d72a35b6c482f4f4adac836a
Bryce Barclay <56102311+BryceBarclay@users.noreply.github.com>
2020-04-30 12:12:26 -0700

    Made progress organizing main_RNN.py. It runs and builds a report of some of the data.

--- Stocks_Project/src/main_RNN.py
+++ Stocks_Project/src/main_RNN.py
@@ -20,10 +20,9 @@ hyperP = {
     'data_to_read': '../dataset/data.hdf5',
     'model': 'keras.layers.LSTM',  # 'LSTM', 'GRU' 
     'hidden_size': 40,
-    'n_steps' : 2001,
     'lr' : .005,
     'sequence_len' : 100,   # x_1 x_2 ... x_100
-    'epochs' : 4,
+    'epochs' : 25,
     #'print_every' : 200,
     #'compute_perplexity_every' : 1000,
     'folder_result': '../results'
@@ -33,8 +32,7 @@ hyperP = {
 #               B) Data/Model/loss                #
 #-------------------------------------------------#
 # get data
-g = h5py.File(hyperP['data_to_read'],"r")#(path + filename, "r")
-print(g.keys())
+g = h5py.File(hyperP['data_to_read'],"r")
 train_input = g["inputs"][:]
 train_labels = g["labels"][:]
 test_input = g["inputs_test"][:]
@@ -43,16 +41,22 @@ test_labels = g["labels_test"][:]
 model = keras.Sequential(
     [eval(hyperP['model'])(16), keras.layers.Dense(1, activation=tf.nn.sigmoid),]
 )
+optim = keras.optimizers.Adam(lr = hyperP['lr'])
 model.compile(
-    optimizer=keras.optimizers.Adam(), loss="binary_crossentropy", metrics=["accuracy"]
+    optimizer=optim, loss="binary_crossentropy", metrics=["accuracy"]
 )
 
 #-------------------------------------------------#
 #                   C) Training                   #
 #-------------------------------------------------#
-t0 = time.time()
-csv_logger = keras.callbacks.CSVLogger(hyperP['folder_result']+'/training.log')
+# create folder to plot/save result
+str_time = datetime.datetime.now().replace(microsecond=0).isoformat(sep='_').replace(':', 'h', 1).replace(':', 'm', 1)
+nameFolder = hyperP['folder_result']+'/Report_'+str_time
+os.makedirs(nameFolder)
+csv_logger = keras.callbacks.CSVLogger(nameFolder+'/training.csv')
 
+t0 = time.time()
+# Train
 train_history = model.fit(train_input, train_labels, epochs=hyperP['epochs'], batch_size=32,callbacks=[csv_logger])
 
 #-------------------------------------------------#
@@ -60,10 +64,6 @@ train_history = model.fit(train_input, train_labels, epochs=hyperP['epochs'], ba
 #-------------------------------------------------#
 time_elapsed = time.time() - t0
 print(' Total time (s) : '+str(time_elapsed))
-# create folder to plot/save result
-str_time = datetime.datetime.now().replace(microsecond=0).isoformat(sep='_').replace(':', 'h', 1).replace(':', 'm', 1)
-nameFolder = hyperP['folder_result']+'/Report_'+str_time
-os.makedirs(nameFolder)
 # D.1) evolution loss/pexplexity 
 loss = train_history.history['loss']
 plt.figure(1);plt.clf()
@@ -71,39 +71,18 @@ plt.plot(loss,'-o',alpha=.2,label='loss')
 plt.grid()
 plt.xlabel('steps')
 plt.legend()
-plt.title('loss/perplexity for the model '+hyperP['model'].replace('_',' '))
-plt.show()
+plt.title('loss for the model '+hyperP['model'].replace('_',' '))
 plt.savefig(nameFolder+'/evolution_loss_perplexity.pdf', bbox_inches='tight', pad_inches=0)
 
+with open(nameFolder+'/hyperParameters.json','w') as jsonFile:
+    json.dump(hyperP, jsonFile, indent=2)
+json_string = model.to_json()
+with open(nameFolder+'/architecture.json','w') as jsonFile:
+    json.dump(json_string, jsonFile, indent=2)
 
-
-#plt.figure(1);plt.clf()
-#plt.plot(df_training['step'],df_training['loss'],'-o',alpha=.2,label='loss')
-#lowess = sm.nonparametric.lowess
-#w = lowess(df_training['loss'], df_training['step'], frac=1/5)
-#plt.plot(w[:,0],w[:,1],'-',color='green',linewidth=2,label='average loss')
-#plt.plot(df_perplex['step'],df_perplex['perplexity'],'-o',label='perplexity')
-#plt.grid()
-#plt.xlabel('steps')
-#plt.legend()
-#plt.title('loss/perplexity for the model '+hyperP['model'].replace('_',' '))
-#plt.savefig(nameFolder+'/evolution_loss_perplexity.pdf', bbox_inches='tight', pad_inches=0)
-
-
-
-
-print(train_input.shape)
-print(train_labels.shape)
-print(np.sum(train_labels) / train_labels.shape[0])
-print(np.sum(test_labels) / test_labels.shape[0])
-
-print(np.sum(train_labels) / train_labels.shape[0])
-
-# evaluate
+# evaluate on test
 test_loss, test_acc = model.evaluate(test_input, test_labels)
 print("test accuracy: ", test_acc)
-print(np.sum(test_labels) / test_labels.shape[0])
 
-
-
+print(model.summary())
 
